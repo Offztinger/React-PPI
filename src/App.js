39d215113dfb131675be190664622806afbee14d
Luis Alejandro Calvo Álvarez <65958264+Offztinger@users.noreply.github.com>
2020-07-21 10:14:18 -0500

    Subiendo React PPI Cambio1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
 import React from "react";
 import "./styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import $ from "jquery";
+import Popper from "popper.js";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    /*Creando la tabla para poner los datos*/
+
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Nombre</th>
+          <th scope="col">Apellido</th>
+          <th scope="col">Correo</th>
+          <th scope="col">Celular</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Alejandro</td>
+          <td>Calvo</td>
+          <td>luiscalvo@inemjose.edu.co</td>
+          <td>3145198712</td>
+        </tr>
+        <tr>
+          <td>Jacobo</td>
+          <td>Uribe</td>
+          <td>jacobouribe@inemjose.edu.co</td>
+          <td>3054332172</td>
+        </tr>
+        <tr>
+          <td>Juan</td>
+          <td>Velasquez</td>
+          <td>juanvelasquez@inemjose.edu.co</td>
+          <td>3057784648</td>
+        </tr>
+        <tr>
+          <td>Samuel</td>
+          <td>Garcia</td>
+          <td>samuelgarcia@inemjose.edu.co</td>
+          <td>3136066855</td>
+        </tr>
+      </tbody>
+    </table>
+
+    /*Cerrando la tabla para poner los datos*/
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import $ from "jquery";
+import Popper from "popper.js";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById("App");
 ReactDOM.render(
   <React.StrictMode>
     <App />
